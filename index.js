193ce0fb5f9fee8b51a454ddf2b937d6fd0a6c1f
mnl187 <mnl187@gmail.com>
2022-08-03 20:34:44 +0200

    added cookie-logo, index.js with all nodes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
+const {configuratorRouter} = require("./routes/configurator");
+const {homeRouter} = require("./routes/home");
+const {orderRouter} = require("./routes/order");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use('/', homeRouter);
+app.use('/configurator', configuratorRouter);
+app.use('/order', orderRouter)
+
+
+
+app.listen(3000, 'localhost');

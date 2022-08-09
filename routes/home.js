23cad0ef1032df8961f7ffd816d3271e562dca5e
mnl187 <mnl187@gmail.com>
2022-08-09 06:59:56 +0200

    adding bases and addons

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const {COOKIE_BASES, COOKIE_ADDONS} = require("../data/cookies-data");
 
 const homeRouter = express.Router();
 
 homeRouter
     .get('/', (req, res) => {
-        res.render('home/index')
+        res.render('home/index', {
+            cookie: {
+                base: 'dark',
+                addons: ['coconut', 'honey'],
+            },
+            bases: Object.entries(COOKIE_BASES),
+            addons: Object.entries(COOKIE_ADDONS),
+            // @TODO: Need smth to know what are the prices of bases and addons
+        })
     })
 module.exports = {
     homeRouter,

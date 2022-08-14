8ff336051effe58f9cc08da648dc6114f46e2412
mnl187 <mnl187@gmail.com>
2022-08-14 14:24:18 +0200

    adding btn-disabled class

--- utils/handlebars-helpers.js
+++ utils/handlebars-helpers.js
@@ -13,6 +13,8 @@ const handlebarsHelpers = {
     pricify: price => price.toFixed(2),
 
     isNotInArray: (array, element) => !array.includes(element),
+
+    isInArray: (array, element) => array.includes(element),
 };
 
 module.exports = {
